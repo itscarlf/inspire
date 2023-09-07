65e39af73b4283366fa9b185b96edd5ade050945
itscarlf <itscarlf@gmail.com>
2023-09-07 12:15:30 +0800

    Develop navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Footer, NavBar } from "@/components";
 
 export const metadata: Metadata = {
   title: "Inspire",
@@ -13,7 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+
+        <main>{children}</main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import AuthProviders from "./AuthProviders";
+
+export { NavBar, Footer, AuthProviders };
